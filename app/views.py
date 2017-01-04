3c58479d9b0a297eb74d7336f63c48a26d7ee7c9
Jacqueline Watts <jacquelineawatts@gmail.com>
2017-01-03 22:10:02 -0800

    Built out new entry form as modal on entries page and integrated into views.

--- app/views.py
+++ app/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Entry
+from .forms import EntryForm
 from django.utils import timezone
+
 
 def entries(request):
     entries = Entry.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    return render(request, 'app/entries.html', {'entries': entries})
+
+    form = EntryForm()
+    if request.method == "POST":
+        form = EntryForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+
+            return redirect('entries')
+
+    return render(request, 'app/entries.html', {'entries': entries, 'form': form})

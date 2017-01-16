22c883e8a94a2c280b343935e576ddcc914f080e
Jacqueline Watts <jacquelineawatts@gmail.com>
2017-01-16 00:02:06 -0800

    Added reprs to models and reset db to clean version. Only contains superuser, no test posts.

--- app/models.py
+++ app/models.py
@@ -15,11 +15,16 @@ class Entry(models.Model):
     def __str__(self):
         return '{}'.format(self.created_date)
 
+
 class Gratitude(models.Model):
 
     author = models.ForeignKey('auth.User')
     text = models.TextField()
     entry = models.ForeignKey(Entry, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'Post by {}:{}'.format(self.author, self.text)
+
 
 class Comment(models.Model):
 
@@ -28,3 +33,6 @@ class Comment(models.Model):
     created_date = models.DateTimeField(
         default=timezone.now)
     gratitude = models.ForeignKey(Gratitude, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'Comment by {}: {}'.format(self.author, self.text)
